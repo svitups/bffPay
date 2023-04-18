08a07e8f05b35fb0757139d3607bad28346d8518
kiddden <kazzer@knu.ua>
2023-04-18 16:42:31 +0300

    Initial Commit

---
+++ bffPay/bffPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  bffPayApp.swift
+//  bffPay
+//
+//  Created by Eugene Ned on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct bffPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
