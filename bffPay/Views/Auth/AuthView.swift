02897f3f8f19c5ce5e4959d895d710c97ff0d267
kiddden <kazzer@knu.ua>
2023-04-22 17:20:08 +0300

    added sign in with Apple, refactored AuthService and AuthView a bit

--- bffPay/Views/Auth/AuthView.swift
+++ bffPay/Views/Auth/AuthView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import SwiftUIFontIcon
+import AuthenticationServices
 
 struct AuthView: View {
     @Environment(\.colorScheme) private var colorScheme
@@ -14,35 +14,79 @@ struct AuthView: View {
     @EnvironmentObject private var authService: AuthService
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Spacer()
-                Text("bffPay")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .bold()
-                Spacer(minLength: geometry.size.height * 0.7)
-                VStack(spacing: 0) {
-                    Group {
-                        Button {
-                            authService.signInWithGoogle()
-                        } label: {
-                            Text("Sign in with Google")
-                                .bold()
-                                .font(.system(size: 18))
-                                .frame(maxWidth: geometry.size.width * 0.7, alignment: .center)
-                                .frame(minHeight: 50)
-                                .background(colorScheme == .dark ? Color.white : Color.black)
-                                .cornerRadius(10)
-                                .foregroundColor(Color(uiColor: .systemBackground))
-                                .padding(20)
-                        }
-                    }
-                }
-                .frame(maxWidth: geometry.size.width)
-                .frame(height: geometry.size.height * 0.1)
-                Spacer()
+        VStack {
+            Spacer()
+            
+            Text("bffPay")
+                .font(.largeTitle)
+                .bold()
+            
+            Spacer()
+            VStack(spacing: 0) {
+                signInWithGoogleButton
+                
+                signInWithAppleButton
             }
         }
+    }
+    
+    private var signInWithGoogleButton: some View {
+        Button {
+            authService.handleSignInWithGoogle()
+        } label: {
+            HStack(spacing: 0) {
+                Image("googleLogo")
+                    .resizable()
+                    .frame(width: Constants.googleLogoSize, height: Constants.googleLogoSize)
+                    .padding(.trailing, 4)
+                
+                Text("Sign in with Google")
+                    .bold()
+                    .font(.system(size: Constants.buttonTextFontSize))
+            }
+            .frame(minWidth: Constants.signInButtonMinWidth, minHeight: Constants.signInButtonMinHeight)
+            .background(colorScheme == .dark ? .white : .black)
+            .cornerRadius(Constants.buttonCornerRadius)
+            .foregroundColor(Color(uiColor: .systemBackground))
+            .padding(Constants.buttonPadding)
+        }
+    }
+    
+    @ViewBuilder
+    private var signInWithAppleButton: some View {
+        // TODO: Standard approach with .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black) doesn't work on current version of iOS
+        VStack {
+            switch colorScheme {
+            case .dark:
+                SignInWithAppleButton { request in
+                    authService.handleSignInWithAppleRequest(request)
+                } onCompletion: { result in
+                    authService.handleSignInWithAppleCompletiion(result)
+                }.signInWithAppleButtonStyle(.white)
+            case .light:
+                SignInWithAppleButton { request in
+                    authService.handleSignInWithAppleRequest(request)
+                } onCompletion: { result in
+                    authService.handleSignInWithAppleCompletiion(result)
+                }.signInWithAppleButtonStyle(.black)
+            @unknown default:
+                fatalError("Not Yet Implemented")
+            }
+        }
+        .frame(maxWidth: Constants.signInButtonMinWidth, maxHeight: Constants.signInButtonMinHeight)
+        .cornerRadius(Constants.buttonCornerRadius)
+        .padding(Constants.buttonPadding)
+    }
+    
+    private enum Constants {
+        static let signInButtonMinHeight: CGFloat = 50
+        static let signInButtonMinWidth: CGFloat = UIScreen.main.bounds.width * 0.7
+        static let buttonCornerRadius: CGFloat = 10
+        static let buttonPadding: CGFloat = 10
+        static let buttonTextFontSize: CGFloat = 18
+        
+        static let googleLogoSize: CGFloat = 18
+        static let googleLogoPadding: CGFloat = 24
     }
 }
 
